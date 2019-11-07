47f04649b7a90f7aeabf64559fb7e31329863f42
freeman6490 <lfreeman6490@gmail.com>
2019-11-06 22:21:24 -0600

    This is the first two files

---
+++ FreemanInsurance/src/FreemanInsurance.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class FreemanInsurance {
+	public static void printWelcome() {
+		System.out.println("*************************************");
+		System.out.println("Insurance Score Card");
+		System.out.println("This app scores a potential customer on");
+		System.out.println(" various health attributes; blood pressure ");
+		System.out.println(", age, height, weight, and family history of ");
+		System.out.println("disease. It writes each member's insurance ");
+		System.out.println("grade to a JSON file so they can be easily");
+		System.out.println(" shared on a web based data exchange.");
+		System.out.println("*************************************");
+	}
+	public static void printMenu() {
+		System.out.println("Here are your choices");
+		System.out.println("1. List members");
+		System.out.println("2. Add a new member");
+		System.out.println("3. Save members");
+		System.out.println("4. Load members");
+		System.out.println("5. Asses members");
+		System.out.println("6. Save assesments as JSON");
+		System.out.println("7. Quit");
+		System.out.println("Enter your choice: ");
+	}
+	public static void main (String [] args) {
+		Scanner sc = new Scanner (System.in);
+		printWelcome();
+		int choice;
+		do {
+			System.out.println("Enter name of file: ");
+			String fileName = sc.nextLine();
+			printMenu();
+			choice = sc.nextInt();
+			if (choice == 1) {
+				
+			} else if (choice == 2) {
+				
+			} else if (choice == 3) {
+				
+			} else if (choice == 4) {
+				
+			} else if (choice == 5) {
+				
+			} else if (choice == 6) {
+				
+			}
+		} while (choice != 7);
+	}
+}
---
+++ FreemanInsurance/src/InsuranceFileReader.java
@@ -0,0 +1,15 @@
+import java.io.File;
+import java.util.*;
+public class InsuranceFileReader {
+	public static ArrayList<String> insuranceFileReader(String fileName) {
+		try {
+			ArrayList<String> rooms = new ArrayList<String>();
+			Scanner fsc = new Scanner (new File(fileName));
+			
+			
+			return null; 	//just for now
+		} catch (Exception ex) {
+			return null;
+		}
+	}
+}
