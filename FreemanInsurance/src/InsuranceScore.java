f301ec4f5d21d8e9ed72fba36313bce52702f258
freeman6490 <lfreeman6490@gmail.com>
2019-11-16 18:27:30 -0600

    fixed most of the assessor class. only have to implement now

--- FreemanInsurance/src/InsuranceScore.java
+++ FreemanInsurance/src/InsuranceScore.java
@@ -1,66 +1,17 @@
-
 public class InsuranceScore {
-	private int score, age, height, weight, sysBP, diaBP;
-	private String verdict, firstName, lastName, cancer, diabetes, alzheimers;
+	private int score;
+	private String verdict, firstName, lastName;
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 	public String getFirstName() {
 		return firstName;
 	}
-	public int getDiaBP() {
-		return diaBP;
-	}
-	public void setDiaBP(int diaBP) {
-		this.diaBP = diaBP;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public int getHeight() {
-		return height;
-	}
-	public void setHeight(int height) {
-		this.height = height;
-	}
-	public int getWeight() {
-		return weight;
-	}
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
-	public int getSysBP() {
-		return sysBP;
-	}
-	public void setSysBP(int sysBP) {
-		this.sysBP = sysBP;
-	}
 	public String getLastName() {
 		return lastName;
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
-	}
-	public String getCancer() {
-		return cancer;
-	}
-	public void setCancer(String cancer) {
-		this.cancer = cancer;
-	}
-	public String getDiabetes() {
-		return diabetes;
-	}
-	public void setDiabetes(String diabetes) {
-		this.diabetes = diabetes;
-	}
-	public String getAlzheimers() {
-		return alzheimers;
-	}
-	public void setAlzheimers(String alzheimers) {
-		this.alzheimers = alzheimers;
 	}
 	public int getScore() {
 		return score;
--- FreemanInsurance/src/MemberAssessor.java
+++ FreemanInsurance/src/MemberAssessor.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Member assessor which is used to decide whether a person can be insured
  * or not, and also determines their risk level
@@ -5,13 +7,79 @@
  */
 public class MemberAssessor {
 	private static int total = 0;
-	private static int bmi = 0;
-	public static int calcBmi(int height, int weight) {
-		//weight in pounds divided by height squared multiplied by 703
-		bmi = (weight / (height * height)) * 703;
-		return bmi;
+	private static int finalTotal = 0;
+	public static int findTotalAge(Customer member) {
+		int age = member.getAge();
+		if (age <= 29) {
+			total = 0;
+		} else if (age <=45) {
+			total = 10;
+		} else if (age <=60) {
+			total = 20;
+		} else {
+			total = 30;
+		}
+		return total;
 	}
+	public static int findTotalDia(Customer member) {
+		String dia = member.getDiabetes();
+		if (dia.equals("n")) {
+			total = 0;
+		} else {
+			total = 10;
+		}
+		return total;
+	}
+	public static int findTotalCan(Customer member) {
+		String can = member.getCancer();
+		if (can.equals("n")) {
+			total = 0;
+		} else {
+			total = 10;
+		}
+		return total;
+	}
+	public static int findTotalAlz(Customer member) {
+		String alz = member.getAlzheimers();
+		if (alz.equals("n")) {
+			total = 0;
+		} else {
+			total = 10;
+		}
+		return total;
+	}
+	public static int findTotalForBP(Customer member) {
+		int sysBP = member.getSysBP();
+		int diaBP = member.getDiaBP();
+		if (sysBP < 120 && diaBP < 80) {
+			total = 0;
+		} else if (sysBP <= 129 && diaBP < 80) {
+			total = 15;
+		} else if (sysBP <= 139 || diaBP < 89) {
+			total = 30;
+		} else if (sysBP >= 140 || diaBP >= 90) {
+			total = 75;
+		} else {
+			total = 100;
+		}
+		return total;
+	}
+	public static int findTotalBmi(Customer member) {
+		int height = member.getHeight();
+		int weight = member.getWeight();
+		int bmi = (weight /(height * height)) * 703;
+		if (bmi >= 18.5 && bmi <= 24.9) {
+			total = 0;
+		} else if (bmi >= 25 && bmi <= 29.9) {
+			total = 30;
+		} else {
+			total = 75;
+		}
+		return total;
+	}
+	/*
 	public static int findTotalScore(int age, int height, int weight, int sysBP, int diaBP, String cancer, String diabetes, String alzheimers) {
+		total = 0;
 		if (age < 30) {
 			total = total;
 		}
@@ -43,18 +111,18 @@ public class MemberAssessor {
 		} else {
 			total = total + 100;
 		}
-		if (cancer == "n") {
+		if (cancer.equals("n")) {
 			total = total;
 		} else {
 			total = total + 10;
 		}
-		if (diabetes == "n") {
+		if (diabetes.equals("n")) {
 			total = total;
 		}
 		else {
 			total = total + 10;
 		}	
-		if (alzheimers == "n") {
+		if (alzheimers.equals("n")) {
 			total = total;
 		}
 		else {
@@ -62,11 +130,34 @@ public class MemberAssessor {
 		}
 		return total;
 	}
+	*/
+	public static void findVerdict(Customer member) {
+		String verdict, stance;
+		int ageTotal = 0, diaTotal = 0, canTotal = 0, BPTotal = 0, alzTotal = 0, bmiTotal = 0;
+		ageTotal = findTotalAge(member);
+		diaTotal = findTotalDia(member);
+		canTotal = findTotalCan(member);
+		BPTotal = findTotalForBP(member);
+		alzTotal = findTotalAlz(member);
+		bmiTotal = findTotalBmi(member);
+		finalTotal = ageTotal + diaTotal + canTotal + BPTotal + alzTotal + bmiTotal;
+		if (finalTotal <= 20) {
+			stance = "low risk";
+			verdict = stance;
+		} else if (finalTotal <= 50) {
+			stance = "moderate risk";
+			verdict = stance;
+		} else if (finalTotal <= 75) {
+			stance = "high risk";
+			verdict = stance;
+		} else {
+			stance = "uninsurable";
+			verdict = stance;
+		}
+		String firstName = member.getFirstName();
+		String lastName = member.getLastName();
+		System.out.printf("for %s %s\n", firstName, lastName);
+		System.out.printf("They had a score of %d\n", finalTotal);
+		System.out.printf("Which makes them %s", verdict);
+	}
 }
-	//public static int memberAssessor(ArrayList<Customer> result) {
-		//int finalTotal = 0;
-		//calcTotalForAge(age);
-		
-	//public static int calcTotal(calcTotalForAge(age),findTotalForBmi(bmi), findTotalForSysBPAndDiaBP(sysBP, diaBP),
-		//	findTotalForCancer(cancer), findTotalForDiabetes(diabetes), findTotalForAlzhemiers(alzheimers)) {
-		 
